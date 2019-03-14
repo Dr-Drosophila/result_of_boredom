db4ea715e75160b000352424aeec2a8e2210a68f
Chinmay Rele <chinmay.rele@rutgers.edu>
2019-03-14 14:44:14 -0400

    Finished Fizzbuzz

--- FizzBuzz/fizzbuzz.py
+++ FizzBuzz/fizzbuzz.py
@@ -39,13 +39,13 @@ if __name__ == "__main__":
 		elif opt in ("-n", "--numbers"):
 			numbers = arg
 		elif opt in ("-l", "--total_length_of_game"):
-			total_length_of_game = arg
+			total_length_of_game = int(arg)
 		elif opt in ("-o", "--output_file"):
-			output_file = arg.upper()
+			output_file = arg
 
-	print( 'numbers is:\t', numbers )
-	print( 'total_length_of_game is:\t\t', total_length_of_game )
-	print( 'output_file is:\t', output_file )
+	print( 'numbers is:\t\t', numbers )
+	print( 'total_length_of_game is:', total_length_of_game )
+	print( 'output_file is:\t\t', output_file )
 
 	print(  )
 
@@ -54,4 +54,35 @@ if __name__ == "__main__":
 """
 
 # words that fizzbuzz is played by
+# 	need to add more to accomodate more numbers
 words = [ "Fizz", "Buzz", "Fooz", "Biff", "Faze", "Booz", "Feet", "Beet", "Fork", "Bork" ]
+
+temp_nums = numbers.split(",")
+numbers = []
+[ numbers.append(int(item)) for item in temp_nums ]
+
+# create ductionary of numbers and words
+num_dict = {}
+for i in range( len(numbers) ):
+	num_dict[ numbers[i] ] = words[i]
+
+print( num_dict )
+
+output_lst = []
+
+# for loop to create the output:
+for i in range( 1, total_length_of_game+1 ):
+	thing = ""
+	for item in list( num_dict.keys() ):
+		if i % item == 0:
+			thing += num_dict[item]
+	if thing == "":
+		thing = str(i)
+	output_lst.append( thing )
+
+print( output_lst )
+
+with open( output_file, "w" ) as out_file:
+	for key, val in num_dict.items():
+		print( "{0}: {1}".format( key, val ), file=out_file )
+	print( file=out_file )
